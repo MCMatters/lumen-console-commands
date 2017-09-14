10422f02aab09142956680524c6e8ea0a6c82517
Dmitry Borzyonok <dima.matters@gmail.com>
2017-09-14 21:53:38 +0300

    Add storage:link command

---
+++ src/Console/Commands/Application/StorageLink.php
@@ -0,0 +1,68 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace McMatters\LumenConsoleCommands\Console\Commands\Application;
+
+use Illuminate\Console\Command;
+use Illuminate\Filesystem\Filesystem;
+use Laravel\Lumen\Application;
+
+/**
+ * Class StorageLink
+ *
+ * @package McMatters\LumenConsoleCommands\Console\Commands\Application
+ */
+class StorageLink extends Command
+{
+    /**
+     * @var string
+     */
+    protected $signature = 'storage:link';
+
+    /**
+     * @var string
+     */
+    protected $description = 'Create a symbolic link from "public/storage" to "storage/app/public"';
+
+    /**
+     * @var Application
+     */
+    protected $app;
+
+    /**
+     * @var Filesystem
+     */
+    protected $files;
+
+    /**
+     * StorageLink constructor.
+     *
+     * @param Application $app
+     */
+    public function __construct(Application $app)
+    {
+        $this->app = $app;
+        $this->files = $app->make('files');
+
+        parent::__construct();
+    }
+
+    /**
+     * @return void
+     */
+    public function handle()
+    {
+        $publicPath = $this->app->basePath('public');
+
+        if (file_exists($publicPath)) {
+            $this->error('The "public/storage" directory already exists.');
+
+            return;
+        }
+
+        $this->files->link($this->app->storagePath('app/public'), $publicPath);
+
+        $this->info('The [public/storage] directory has been linked.');
+    }
+}
